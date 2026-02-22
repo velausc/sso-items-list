ace8c2bc90848548cfa75d853a50e37e2931f050
velausc <karzis002@gmail.com>
2026-02-22 01:14:41 +0100

    Added new items, updated code

---
+++ sw.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker Registered!', reg))
+      .catch(err => console.log('Registration Failed', err));
+  });
+}
